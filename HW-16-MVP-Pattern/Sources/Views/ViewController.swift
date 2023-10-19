9ac4d27b531dee8752346e359bf60e27d36701a2
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-10-19 22:45:18 +0300

    add model button

--- HW-16-MVP-Pattern/Sources/Views/ViewController.swift
+++ HW-16-MVP-Pattern/Sources/Views/ViewController.swift
@@ -15,10 +15,19 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Архитектурный паттерн MVP"
         label.font = .boldSystemFont(ofSize: 26)
-        label.textColor = .black
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+    
+    private lazy var modelButton: UIButton = {
+        let modelButton = UIButton(type: .system)
+        modelButton.setTitle("Model", for: .normal)
+        modelButton.backgroundColor = .init(red: 214 / 255, green: 232 / 255, blue: 213 / 255, alpha: 1)
+        modelButton.layer.cornerRadius = 15
+        modelButton.translatesAutoresizingMaskIntoConstraints = false
+        return modelButton
+        
     }()
     
     // MARK: Lifecycle
@@ -38,7 +47,7 @@ class ViewController: UIViewController {
     }
     
     private func setupHierarchy() {
-        let views = [titleLabel]
+        let views = [titleLabel, modelButton]
         views.forEach { view.addSubview($0) }
     }
     
@@ -46,7 +55,11 @@ class ViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            modelButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
+            modelButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            modelButton.widthAnchor.constraint(equalToConstant: 120),
+            modelButton.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
 
